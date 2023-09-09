b28f54d9de6ad10e8bbced75b76aa108a19280a8
Chris Jason Oswa <chrisjasonoswa555@gmail.com>
2023-09-10 00:13:10 +0800

    Updated requirements

--- petsis/settings.py
+++ petsis/settings.py
@@ -16,6 +16,9 @@ import os
 # import django_heroku
 # import dj_database_url
 
+#FOR CSRF verification
+CSRF_TRUSTED_ORIGINS = ['https://petsis-production.up.railway.app/']
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
